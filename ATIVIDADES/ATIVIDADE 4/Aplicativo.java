d73039b0df4db59609b74b8d0038b8d87e92bb3d
Izabela Cristina <izabelacfw@gmail.com>
2024-11-15 12:02:11 -0300

    atividade contabanco

---
+++ ATIVIDADES/ATIVIDADE 4/Aplicativo.java
@@ -0,0 +1,23 @@
+public class Aplicativo {
+    public static void main(String[] args) {
+        ContaBanco p1= new ContaBanco(0, false);
+        p1.setNumConta(1111);
+        p1.setDono("Jubileu");
+        p1.abrirConta("CC");
+
+        ContaBanco p2= new ContaBanco(0, false);
+        p2.setNumConta(2222);
+        p2.setDono("Creuza");
+        p2.abrirConta("CP");
+        
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(1000);
+
+        p1.sacar(150);
+        p1.fecharConta();
+
+        p1.estadoAtual();
+        p2.estadoAtual();
+    }
+}
---
+++ ATIVIDADES/ATIVIDADE 4/ContaBanco.java
@@ -0,0 +1,121 @@
+public class ContaBanco {
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    public ContaBanco (float saldo, boolean status){
+        setSaldo(0);
+        setStatus(false);
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void abrirConta(String tipo){
+        setTipo(tipo);
+        setStatus(true);{
+            if (getTipo().equals("CC")){
+                setSaldo(50);
+            }else if(getTipo().equals("CP")){
+                setSaldo(150);
+            }
+            System.out.println("Conta aberta com sucesso!");
+        }
+    }
+
+    public void fecharConta(){
+        if( getSaldo()>0){
+            System.out.println("Conta com dinheiro");
+        }else if (getSaldo()<0) {
+            System.out.println("Conta em débito");
+        }else{
+            setStatus(false);
+            System.out.println("Conta fechada com sucesso.");
+        }
+    }
+
+    public void depositar(float v){
+        if(isStatus()){
+            setSaldo(getSaldo()+v);
+            System.out.println("Depósito realizado na conta de "+getDono());
+        }else{
+            System.out.println("Impossivel depositar em uma conta fechada");
+        }
+    }
+
+    public void sacar(float v){
+        if(isStatus()){
+            if(getSaldo()>=v){
+                setSaldo(getSaldo()-v);
+                System.out.println("Saque realizado na conta de "+getDono());
+            }else{
+                System.out.println("Saldo insuficiente");
+            }
+        }else{
+            System.out.println("Impossivel sacar de uma conta fechada");
+        }
+    }
+
+    public void pagarMensal(){
+        int v=0;
+        if(getTipo()=="CC"){
+            v=12;
+        }else if(getTipo()=="CP"){
+            v=20;
+        }else{
+            if(isStatus()){
+                if(getSaldo()>v){
+                    setSaldo(getSaldo()-v);
+                }
+            }
+        }
+    }
+
+    public void estadoAtual(){
+        System.out.println("--------------------------");
+        System.out.println("Conta: "+getNumConta());
+        System.out.println("Tipo: "+getTipo());
+        System.out.println("Dono: "+getDono());
+        System.out.println("Saldo: "+getSaldo());
+        System.out.println("Status: "+isStatus());
+    }
+}   
